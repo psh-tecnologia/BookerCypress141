20e5c5347d94fe51c9d38f776e751942a132ff8e
paulohqsouza <110414038+paulohqsouza@users.noreply.github.com>
2024-08-22 22:33:50 -0300

    Patch e Delete

--- cypress/e2e/booker.cy.js
+++ cypress/e2e/booker.cy.js
@@ -1,5 +1,6 @@
 //bibliotecas / imports
 import booking from '../fixtures/booking.json'
+import update from '../fixtures/update.json'
 
 describe('Booker API', () => {
 
@@ -21,7 +22,40 @@ describe('Booker API', () => {
       expect(body.booking.depositpaid).to.eq(true)
       expect(body.booking.bookingdates.checkin).to.eq('2024-08-23')
       expect(body.booking.bookingdates.checkout).to.eq('2024-08-25')
-      expect(body.booking.additionalneeds.to.eq('Breakfast')
+      expect(body.booking.additionalneeds).to.eq('Breakfast')
+    })
+  })
+
+  // Update parcial
+  it('Patch Booking', () => {
+    cy.request({
+      method: 'PATCH',
+      url: `/booking/${Cypress.env('bookingid')}`,
+      body: update,
+      headers: {
+        Cookie: `token=${Cypress.env('token')}`
+      }
+    }).then(({ status, body}) => {
+      expect(status).to.eq(200)
+      expect(body.firstname).to.eq('Paulo')
+      expect(body.lastname).to.eq('Souza')
+      expect(body.totalprice).to.eq(500)
+      expect(body.depositpaid).to.eq(true)
+      expect(body.bookingdates.checkin).to.eq('2024-08-23')
+      expect(body.bookingdates.checkout).to.eq('2024-08-25')
+      expect(body.additionalneeds).to.eq('Dinner')
+    })
+  }) // termina patch
+
+  it('Delete Booking', () =>{
+    cy.request({
+      method: 'DELETE',
+      url: `/booking/${Cypress.env('bookingid')}`,
+      headers: {
+        Cookie: `token=${Cypress.env('token')}`
+      }
+    }).then(({status}) => {
+      expect(status).to.eq(201)
     })
   })
 
